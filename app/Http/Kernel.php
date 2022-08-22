c83df76bb746ae488587e26249ee66e0a7bc2272
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2022-08-21 22:09:17 -0400

    middleware to enable x origin requests

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'subscribed' => \App\Http\Middleware\CheckSubscriptionStatus::class,
+        'xframe.options' => \App\Http\Middleware\XFrameOptions::class,
     ];
 }
