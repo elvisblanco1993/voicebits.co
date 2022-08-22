e715749b58a0f60316d77d1654ba05f5b54c6605
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2022-08-21 22:42:07 -0400

    ..

--- app/Http/Middleware/XFrameOptions.php
+++ app/Http/Middleware/XFrameOptions.php
@@ -17,19 +17,7 @@ class XFrameOptions
     public function handle(Request $request, Closure $next)
     {
         $response = $next($request);
-        $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
-        $allow_origin = [
-            'https://voicebits.co',
-            'https://*.voicebits.co'
-        ];
-        if (in_array($origin, $allow_origin)) {
-            // $response->header('Access-Control-Allow-Origin', $origin);
-            // $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN');
-            // $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
-            // $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
-            // $response->header('Access-Control-Allow-Credentials', 'true');
-            $response->header('X-Frame-Options: SAMEORIGIN');
-        }
+        $response->header('X-Frame-Options: SAMEORIGIN');
         return $response;
     }
 }
