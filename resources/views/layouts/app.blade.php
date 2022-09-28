d6534f425851468ac8c18a6e3479351819f8b943
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2022-09-27 23:23:38 -0400

    ..

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
                         <main class="col-span-4 md:col-span-3">
                             {{-- Trial --}}
                             @if (Auth::user()->onTrial() && !request()->routeIs('signup'))
-                                <div class="mb-4 w-full px-4 py-1.5 text-sm bg-slate-200 text-center rounded-full border border-slate-300">
+                                <div class="mb-4 w-full px-4 py-1.5 text-sm bg-slate-200 text-center rounded-lg border border-slate-300">
                                     You have {{ abs(round((strtotime(Auth::user()->trial_ends_at) - strtotime(now()))/86400)) }} days left on your trial. If you are enjoying Voicebits, you can <a href="{{ route('signup') }}" class="text-indigo-600 bg-indigo-200 px-1 rounded border-b border-b-indigo-400">sign up here.</a> 🚀
                                 </div>
                             @endif
