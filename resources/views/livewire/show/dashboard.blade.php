17d847be568f3b81d1a6f9fc8e92ec3bb3d8959c
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2022-09-28 10:52:39 -0400

    Dashboard redesign

--- resources/views/livewire/show/dashboard.blade.php
+++ resources/views/livewire/show/dashboard.blade.php
@@ -9,18 +9,20 @@
 
         {{-- Statistics --}}
         @if ($podcast->episodes->count() > 0)
-            <div class="mt-10 grid grid-cols-3 gap-10">
-                <div class="col-span-3 sm:col-span-1">
+            <div class="mt-10 grid grid-cols-2 gap-8">
+                <div class="col-span-2 sm:col-span-1">
                     @livewire('show.statistics.total-plays', ['podcast' => $podcast->id])
                 </div>
-                <div class="col-span-3 sm:col-span-1">
+                <div class="col-span-2 sm:col-span-1">
                     @livewire('show.statistics.estimated-audience-size', ['podcast' => $podcast->id])
                 </div>
-                <div class="col-span-3 sm:col-span-1">
+                <div class="col-span-2 sm:col-span-1">
                     @livewire('show.statistics.plays-at-show-launch', ['podcast' => $podcast->id])
                 </div>
+                <div class="col-span-2 sm:col-span-1">
+                    @livewire('show.statistics.total-plays-by-region', ['podcast' => $podcast->id])
+                </div>
             </div>
-            @livewire('show.statistics.total-plays-by-region', ['podcast' => $podcast->id])
         @endif
     </div>
 </div>
--- resources/views/livewire/show/statistics/total-plays.blade.php
+++ resources/views/livewire/show/statistics/total-plays.blade.php
@@ -1,13 +1,19 @@
 <div>
-    <div class="w-full py-12 px-8 bg-white rounded-lg shadow">
-        <div class="flex items-center justify-between">
-            <div>
-                <div class="text-xs tracking-wide font-mono font-semibold text-slate-400">Website <span class="ml-2">{{ $from_website }}</span></div>
-                <div class="text-xs tracking-wide font-mono font-semibold text-slate-400">RSS Players <span class="ml-2">{{ $from_third_parties }}</span></div>
+    <div class="w-full p-8 bg-white rounded-lg shadow">
+        <div class="py-2 border-b flex items-center justify-between" title="This are the overall podcast reproductions from your feed and website, and third party podcast distributors, such as Spotify, Apple Podcasts, Google Podcasts, etc...">
+            <h1 class="font-semibold">Overall Reproductions</h1>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 hover:cursor-pointer hover:text-indigo-600">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
+            </svg>
+        </div>
+        <div class="grid grid-cols-2 gap-2 py-8">
+            <div class="col-span-2 sm:col-span-1">
+                <p class="font-normal text-xl text-indigo-600 text-center">{{ $from_third_parties }}</p>
+                <p class="text-sm font-normal text-slate-500 text-center">Podcatchers</p>
             </div>
-            <div class="text-right">
-                <div class="text-2xl font-bold">{{ $totalPlays }}</div>
-                <div class="text-sm font-semibold text-slate-500">Overall plays</div>
+            <div class="col-span-2 sm:col-span-1">
+                <p class="font-normal text-xl text-indigo-600 text-center">{{ $from_website }}</p>
+                <p class="text-sm font-normal text-slate-500 text-center">RSS/Website</p>
             </div>
         </div>
     </div>
