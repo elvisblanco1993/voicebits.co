5909172947d6b7d7178d8b0581169dc8b5f76181
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2022-12-19 23:28:36 -0500

    opens podcatcher link on new tab

--- resources/views/web/templates/classic/distributors.blade.php
+++ resources/views/web/templates/classic/distributors.blade.php
@@ -1,43 +1,43 @@
 @if ($podcast->apple)
-<a href="{{ $podcast->apple }}" class="podcatcher-link">Listen on Apple Podcasts</a>
+<a href="{{ $podcast->apple }}" target="_blank" class="podcatcher-link">Listen on Apple Podcasts</a>
 @endif
 @if ($podcast->spotify)
-<a href="{{ $podcast->spotify }}" class="podcatcher-link">Listen on Spotify</a>
+<a href="{{ $podcast->spotify }}" target="_blank" class="podcatcher-link">Listen on Spotify</a>
 @endif
 
 @if ($podcast->google)
-<a href="{{ $podcast->google }}" class="podcatcher-link">Listen on Google Podcasts</a>
+<a href="{{ $podcast->google }}" target="_blank" class="podcatcher-link">Listen on Google Podcasts</a>
 @endif
 
 @if ($podcast->stitcher)
-<a href="{{ $podcast->stitcher }}" class="podcatcher-link">Listen on Stitcher</a>
+<a href="{{ $podcast->stitcher }}" target="_blank" class="podcatcher-link">Listen on Stitcher</a>
 @endif
 
 @if ($podcast->pocketcasts)
-<a href="{{ $podcast->pocketcasts }}" class="podcatcher-link">Listen on Pocket Casts</a>
+<a href="{{ $podcast->pocketcasts }}" target="_blank" class="podcatcher-link">Listen on Pocket Casts</a>
 @endif
 
 @if ($podcast->amazon)
-<a href="{{ $podcast->amazon }}" class="podcatcher-link">Listen on Amazon Music</a>
+<a href="{{ $podcast->amazon }}" target="_blank" class="podcatcher-link">Listen on Amazon Music</a>
 @endif
 
 @if ($podcast->pandora)
-<a href="{{ $podcast->pandora }}" class="podcatcher-link">Listen on Pandora</a>
+<a href="{{ $podcast->pandora }}" target="_blank" class="podcatcher-link">Listen on Pandora</a>
 @endif
 
 @if ($podcast->iheartradio)
-<a href="{{ $podcast->iheartradio }}" class="podcatcher-link">Listen on iHeartRadio</a>
+<a href="{{ $podcast->iheartradio }}" target="_blank" class="podcatcher-link">Listen on iHeartRadio</a>
 @endif
 
 @if ($podcast->castbox)
-<a href="{{ $podcast->castbox }}" class="podcatcher-link">Listen on Castbox</a>
+<a href="{{ $podcast->castbox }}" target="_blank" class="podcatcher-link">Listen on Castbox</a>
 @endif
 
 @if ($podcast->deezer)
-<a href="{{ $podcast->deezer }}" class="podcatcher-link">Listen on Deezer</a>
+<a href="{{ $podcast->deezer }}" target="_blank" class="podcatcher-link">Listen on Deezer</a>
 @endif
 
 @if ($podcast->castro)
-<a href="{{ $podcast->castro }}" class="podcatcher-link">Listen on Castro</a>
+<a href="{{ $podcast->castro }}" target="_blank" class="podcatcher-link">Listen on Castro</a>
 @endif
-<a href="{{ route('show.feed', ['url' => $podcast->url, 'player' => 'rss']) }}" class="podcatcher-link">RSS Feed</a>
+<a href="{{ route('show.feed', ['url' => $podcast->url, 'player' => 'rss']) }}" target="_blank" class="podcatcher-link">RSS Feed</a>
