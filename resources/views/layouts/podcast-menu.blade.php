fcf79b6555f4e6916dc798560d84ba7a4663754d
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-01-28 21:19:54 -0500

    ..

--- resources/views/layouts/podcast-menu.blade.php
+++ resources/views/layouts/podcast-menu.blade.php
@@ -1,10 +1,10 @@
-<div class="w-full h-64 bg-gradient-to-tr from-slate-300 to-slate-200 text-slate-900 rounded-2xl">
-    <div class="w-full h-full flex items-center justify-between space-x-6 px-4 sm:px-6 lg:px-8">
+<div class="w-full h-64 bg-gradient-to-tr from-slate-300 to-slate-200 text-slate-900 rounded-xl bg-center bg-cover" @if($podcast->cover)style="background-image: url({{ Storage::url($podcast->cover) }})"@endif>
+    <div class="w-full h-full bg-white/40 backdrop-blur-lg rounded-xl flex items-center justify-between space-x-6 px-4 sm:px-6 lg:px-8">
         <div class="">
             <h1 class="text-4xl font-extrabold">{{ $podcast->name }}</h1>
             <p class="mt-2 text-xl font-semibold">with {{ $podcast->author }}</p>
         </div>
-        <div class="flex-none items-center justify-center">
+        <div class="hidden sm:block sm:flex-none items-center justify-center">
             @if ($podcast->cover)
                 <img src="{{ Storage::url($podcast->cover) }}" class="flex-none h-40 w-40 aspect-square object-center object-cover rounded-full">
             @else
@@ -16,7 +16,7 @@
     </div>
 </div>
 
-<div class="mt-4 flex items-center justify-between overflow-auto py-2 ">
+<div class="py-4 flex items-center justify-between overflow-auto">
     <div class="flex items-center space-x-4">
         <a href="{{ route('show', ['show' => $podcast->id]) }}" @class([
             'flex items-center w-full text-center text-sm font-semibold hover:text-blue-600 transition-all',
@@ -58,7 +58,7 @@
             ])>Team</a>
         @endcan
     </div>
-    <div class="">
+    <div class="ml-4">
         @can('edit_podcast', $podcast)
             <a href="{{ route('show.settings', ['show' => $podcast->id]) }}"@class([
                 'flex items-center w-full text-center text-sm font-semibold hover:text-blue-600 transition-all',
