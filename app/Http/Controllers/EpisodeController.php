8048d711820cab25859ffed668054aff0bc76972
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-02-28 08:49:43 -0500

    removed unnecessary code

--- app/Http/Controllers/EpisodeController.php
+++ app/Http/Controllers/EpisodeController.php
@@ -81,17 +81,4 @@ class EpisodeController extends Controller
             'embed_url' => route('episode.embed', ['guid' => $guid, 'player' => $player]),
         ]);
     }
-
-    private function cidr_match($ip, $range)
-    {
-        list ($subnet, $bits) = explode('/', $range);
-        if ($bits === null) {
-            $bits = 32;
-        }
-        $ip = ip2long($ip);
-        $subnet = ip2long($subnet);
-        $mask = -1 << (32 - $bits);
-        $subnet &= $mask; # nb: in case the supplied subnet wasn't correctly aligned
-        return ($ip & $mask) == $subnet;
-    }
 }
