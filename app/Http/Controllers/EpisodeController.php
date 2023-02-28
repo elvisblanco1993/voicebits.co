dedbe1db4826199ea18040a256d9ef1488467f78
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-02-28 00:47:19 -0500

    ..

--- app/Http/Controllers/EpisodeController.php
+++ app/Http/Controllers/EpisodeController.php
@@ -28,7 +28,7 @@ class EpisodeController extends Controller
     public function play($url, $episode, $player)
     {
         $episode = Episode::where('guid', $episode)->first();
-        // Only count plays here when playing from Third Party player.
+        // Only count plays here when playing from Third Party player or when not requesting from Apple Servers.
         if ($player != 'web' || !$this->cidr_match(Location::get()->ip, '17.58.59.0/24')) {
             (new PlaysCounterController)->playCounter($episode->id, $episode->podcast_id, $player);
         } else {
