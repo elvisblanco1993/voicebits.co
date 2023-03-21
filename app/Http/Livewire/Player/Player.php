2e65bc13e05d4dd4a11aa51c88b5edabcc7166d5
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-03-21 14:34:23 -0400

    ..

--- app/Http/Livewire/Player/Player.php
+++ app/Http/Livewire/Player/Player.php
@@ -20,7 +20,7 @@ class Player extends Component
     {
         $this->episode = Episode::where('guid', $guid)->first();
         // $url = route('public.episode.play', ['url' => $this->episode->podcast->url, 'episode' => $guid, 'player' => 'web']);
-        $url = config('app.url') . "/s/$this->episode->podcast->url/play/$guid/web.mp3";
+        $url = config('app.url') . "/s/" . $this->episode->podcast->url . "/play/$guid/web.mp3";
         $this->emit('gotEpisodeData', $url, $this->episode->title, $guid);
     }
 
