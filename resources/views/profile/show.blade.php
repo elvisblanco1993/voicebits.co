bce22cb7e91e27670c49a0cf98dd4defbe2f7679
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-03-21 14:11:31 -0400

    ..

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,41 +1,43 @@
 <x-app-layout>
-    <h1 class="text-4xl font-bold">Profile</h1>
+    <div class="py-12">
+        <h1 class="text-4xl font-bold">Profile</h1>
 
-    <div>
-        <div class="max-w-5xl mx-auto py-6 sm:px-6 lg:px-0">
-            @if (Laravel\Fortify\Features::canUpdateProfileInformation())
-                @livewire('profile.update-profile-information-form')
+        <div>
+            <div class="max-w-5xl mx-auto py-6 sm:px-6 lg:px-0">
+                @if (Laravel\Fortify\Features::canUpdateProfileInformation())
+                    @livewire('profile.update-profile-information-form')
 
-                <x-section-border />
-            @endif
+                    <x-section-border />
+                @endif
 
-            @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
+                @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
+                    <div class="mt-10 sm:mt-0">
+                        @livewire('profile.update-password-form')
+                    </div>
+
+                    <x-section-border />
+                @endif
+
+                @if (Laravel\Fortify\Features::canManageTwoFactorAuthentication())
+                    <div class="mt-10 sm:mt-0">
+                        @livewire('profile.two-factor-authentication-form')
+                    </div>
+
+                    <x-section-border />
+                @endif
+
                 <div class="mt-10 sm:mt-0">
-                    @livewire('profile.update-password-form')
+                    @livewire('profile.logout-other-browser-sessions-form')
                 </div>
 
-                <x-section-border />
-            @endif
+                @if (Laravel\Jetstream\Jetstream::hasAccountDeletionFeatures())
+                    <x-section-border />
 
-            @if (Laravel\Fortify\Features::canManageTwoFactorAuthentication())
-                <div class="mt-10 sm:mt-0">
-                    @livewire('profile.two-factor-authentication-form')
-                </div>
-
-                <x-section-border />
-            @endif
-
-            <div class="mt-10 sm:mt-0">
-                @livewire('profile.logout-other-browser-sessions-form')
+                    <div class="mt-10 sm:mt-0">
+                        @livewire('profile.delete-user-form')
+                    </div>
+                @endif
             </div>
-
-            @if (Laravel\Jetstream\Jetstream::hasAccountDeletionFeatures())
-                <x-section-border />
-
-                <div class="mt-10 sm:mt-0">
-                    @livewire('profile.delete-user-form')
-                </div>
-            @endif
         </div>
     </div>
 </x-app-layout>
