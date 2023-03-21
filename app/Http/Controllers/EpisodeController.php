ef64fe0dc53c154e9f48227e9cb0e89faea189b4
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-03-21 00:35:50 -0400

    Apple still no-go

--- app/Http/Controllers/EpisodeController.php
+++ app/Http/Controllers/EpisodeController.php
@@ -29,9 +29,9 @@ class EpisodeController extends Controller
     {
         $episode = Episode::where('guid', $episode)->first();
         // Only count plays here when playing from Third Party player or when not requesting from Apple Servers.
-        // if ($player != 'apple.mp3') {
+        if ($player != 'apple.mp3') {
             (new PlaysCounterController)->playCounter($episode->id, $episode->podcast_id, $player);
-        // }
+        }
 
         $file = Storage::disk(config('filesystems.default'))->get($episode->track_url);
         $size = Storage::disk(config('filesystems.default'))->size($episode->track_url);
