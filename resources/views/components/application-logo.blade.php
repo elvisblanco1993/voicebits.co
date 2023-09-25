9286f8387f26c85be47069c7dbd779ae0e77dc55
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-09-24 22:12:34 -0400

    ..

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,2 +1,4 @@
-<img src="{{ asset('logo-mark-dark.svg') }}" alt="" class="block h-6 w-auto">
-<span class="text-xl font-medium text-slate-800">Voicebits</span>
+<div class="flex items-center space-x-4">
+    <img src="{{ asset('logo-mark-dark.svg') }}" alt="" class="h-6 w-auto">
+    <span class="text-xl font-medium text-slate-800">Voicebits</span>
+</div>
