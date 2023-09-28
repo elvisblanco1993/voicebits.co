809e80fc0ef45c8a4cbcf3993a709c662c09618a
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-09-28 17:44:44 -0400

    ..

--- resources/views/livewire/show/social.blade.php
+++ resources/views/livewire/show/social.blade.php
@@ -2,38 +2,38 @@
     @livewire('submenu')
     <div class="py-6">
         <div class="text-2xl font-bold">Social media</div>
-        <div class="mt-4 pt-8 w-full sm:w-2/3 bg-white rounded-lg shadow">
+        <div class="mt-4 pt-8 w-full bg-white rounded-lg shadow">
             <div class=" px-6">
                 <x-label for="twitter" value="Twitter" class=""/>
-                <x-input id="twitter" type="url" wire:model.live="twitter" pattern="https://.*" class="w-full"/>
+                <x-input id="twitter" type="url" wire:model.live="twitter" pattern="https://.*" class="w-full mt-1"/>
             </div>
             <div class="mt-6 px-6">
                 <x-label for="instagram" value="Instagram" class=""/>
-                <x-input id="instagram" type="url" wire:model.live="instagram" pattern="https://.*" class="w-full"/>
+                <x-input id="instagram" type="url" wire:model.live="instagram" pattern="https://.*" class="w-full mt-1"/>
             </div>
             <div class="mt-6 px-6">
                 <x-label for="facebook" value="Facebook"/>
-                <x-input id="facebook" type="url" wire:model.live="facebook" pattern="https://.*" class="w-full"/>
+                <x-input id="facebook" type="url" wire:model.live="facebook" pattern="https://.*" class="w-full mt-1"/>
             </div>
             <div class="mt-6 px-6">
                 <x-label for="linkedin" value="Linkedin"/>
-                <x-input id="linkedin" type="url" wire:model.live="linkedin" pattern="https://.*" class="w-full"/>
+                <x-input id="linkedin" type="url" wire:model.live="linkedin" pattern="https://.*" class="w-full mt-1"/>
             </div>
             <div class="mt-6 px-6">
                 <x-label for="pinterest" value="Pinterest"/>
-                <x-input id="pinterest" type="url" wire:model.live="pinterest" pattern="https://.*" class="w-full"/>
+                <x-input id="pinterest" type="url" wire:model.live="pinterest" pattern="https://.*" class="w-full mt-1"/>
             </div>
             <div class="mt-6 px-6">
                 <x-label for="youtube" value="Youtube"/>
-                <x-input id="youtube" type="url" wire:model.live="youtube" pattern="https://.*" class="w-full"/>
+                <x-input id="youtube" type="url" wire:model.live="youtube" pattern="https://.*" class="w-full mt-1"/>
             </div>
             <div class="mt-6 px-6">
                 <x-label for="vimeo" value="Vimeo"/>
-                <x-input id="vimeo" type="url" wire:model.live="vimeo" pattern="https://.*" class="w-full"/>
+                <x-input id="vimeo" type="url" wire:model.live="vimeo" pattern="https://.*" class="w-full mt-1"/>
             </div>
             <div class="mt-6 px-6">
                 <x-label for="medium" value="Medium"/>
-                <x-input id="medium" type="url" wire:model.live="medium" pattern="https://.*" class="w-full"/>
+                <x-input id="medium" type="url" wire:model.live="medium" pattern="https://.*" class="w-full mt-1"/>
             </div>
 
             <div class="mt-6 bg-slate-100 px-8 py-4 flex items-center justify-end">
