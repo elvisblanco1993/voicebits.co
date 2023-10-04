dace2c0f4f5cbf6e445a17f4e4aeb002a5594b4e
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-10-04 12:20:21 -0400

    ..

--- resources/views/website/partials/footer.blade.php
+++ resources/views/website/partials/footer.blade.php
@@ -1,6 +1,6 @@
-<footer class="mt-20 w-full bg-gray-100">
+<footer class="mt-20 w-full bg-slate-200">
     <div class="max-w-screen-2xl mx-auto">
-        <div class="text-center text-sm font-light text-slate-600 px-4 sm:px-6 lg:px-8 py-6">
+        <div class="text-center text-sm font-light text-slate-800 px-4 sm:px-6 lg:px-8 py-6">
             <span class="block">© 2019 - {{ date("Y") }} Voicebits. All rights reserved.</span>
         </div>
     </div>
