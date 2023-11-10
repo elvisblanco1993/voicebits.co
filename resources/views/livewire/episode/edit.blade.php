c9db74c3b25acbe821a67ae1dc3a177826c0be96
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-11-10 10:31:45 -0500

    Added episode guid

--- resources/views/livewire/episode/edit.blade.php
+++ resources/views/livewire/episode/edit.blade.php
@@ -7,7 +7,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round" d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z" />
                 </svg>
             </a>
-            <div class="text-2xl font-bold">Editing episode</div>
+            <div class="text-2xl font-bold">Editing episode <span class="text-sm text-slate-500">{{ $episode->guid }}</span></div>
         </div>
         <div class="mt-4 w-full bg-white rounded-lg shadow">
             <div class="p-6">
